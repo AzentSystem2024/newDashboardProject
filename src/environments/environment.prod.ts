e2be36f4c120cecf60cb5a6da137da823d22f3b5
NithinShivadas <nithinsivadas1710@gmail.com>
2025-01-20 17:09:16 +0530

    bootstrap packeges installed directly and bug fixing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  LMS_API_BASE_URL: 'http://103.180.120.134/dashboardapi/api'
+  LMS_API_BASE_URL: 'http://192.168.60.167/dashboardapi/api/',
 };
-
-
